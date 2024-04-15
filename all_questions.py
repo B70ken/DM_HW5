56104f49f0db219dd1b94e3b8a536ea8e0ebfabc
B70ken <157922996+B70ken@users.noreply.github.com>
2024-04-15 18:10:35 -0400

    Add files via upload

---
+++ all_questions.py
@@ -0,0 +1,261 @@
+import pytest
+from all_questions import *
+import pickle
+
+
+
+#-----------------------------------------------------------
+def question1():
+    answers = {}
+
+    # type: float
+    # Calculate the probability.
+    answers['(a)'] = 0.0288
+
+    # type: float
+    # Calculate the probability.
+    answers['(b)'] = 0.002
+
+    # type: float
+    # Calculate the probability.
+    answers['(c)'] = 0.08
+    return answers
+
+
+#-----------------------------------------------------------
+def question2():
+    answers = {}
+
+    # type: bool
+    answers['(a) A'] = True
+
+    # type: bool
+    answers['(a) B'] = False
+
+    # type: bool
+    answers['(a) C'] = False
+
+    # type: bool
+    answers['(a) D'] = True
+
+    # type: bool
+    answers['(b) A'] = True
+
+    # type: False
+    answers['(b) B'] = False
+
+    # type: bool
+    answers['(b) C'] = True
+
+    # type: bool
+    answers['(b) D'] = False
+
+    # type: eval_float
+    # The formulas should only use the variable 'p'. The formulas should be
+    # a valid Python expression. Use the functions in the math module as
+    # required.
+    answers['(c) Weight update'] = 0.424
+
+    # type: float
+    # the answer should be correct to 3 significant digits
+    answers['(d) Weight influence'] = 1.53
+    return answers
+
+
+#-----------------------------------------------------------
+def question3():
+    answers = {}
+
+    # type: string
+    answers['Agree?'] = 'No'
+
+    # type: explain_string
+    answers['Explain'] = "There are two problems with Alan's hypothesis of using coin flips to anticipate the stock market. Since coin flips are random events, a series of flips cannot be regarded as a predictive model since it lacks a predictive signal. Furthermore, since the two are unrelated, the economy cannot be forecast by a coin flip.." 
+    return answers
+
+
+#-----------------------------------------------------------
+def question4():
+    answers = {}
+
+    # type: bool
+    answers['(a) e=0.5, independent'] = False
+
+    # type: bool
+    answers['(b), independent'] = True
+
+    # type: bool
+    answers['(c) identical'] = False
+    return answers
+
+
+#-----------------------------------------------------------
+def question5():
+    answers = {}
+
+    # type: string
+    # choices: ['i', 'ii', 'iii', 'iv']
+    answers['(a)'] = 'iii'
+
+    # type: string
+    # choices: ['i', 'ii', 'iii', 'iv']
+    answers['(b)'] = 'i'
+
+    # type: string
+    # choices: ['i', 'ii', 'iii', 'iv']
+    answers['(c)'] = 'ii'
+
+    # type: string
+    # choices: ['i', 'ii', 'iii', 'iv']
+    answers['(d)'] = 'iv'
+    return answers
+
+
+#-----------------------------------------------------------
+def question6():
+    answers = {}
+
+    # type: eval_float
+    answers['(a) C1-TPR'] = 'p'
+
+    # type: eval_float
+    answers['(a) C2-TPR'] = '2 * p'
+
+    # type: eval_float
+    answers['(a) C1-FPR'] = 'p'
+
+    # type: eval_float
+    answers['(a) C2-FPR'] = '2 * p'
+
+    # type: string
+    # Hint: The random guess line in an ROC curve corresponds to TPR=FPR.
+    # choices: ['yes', 'no']
+    answers['(b) C2 better classifier than C1?'] = 'no'
+
+    # type: explain_string
+    answers['(b) C2 better classifier than C1? Explain'] = "C2 has a higher FPR in addition to a higher TPR. Predicting more good results comes with a risk of producing more false positive results. Both of the classifiers have the same ratio of TP to FP."
+
+    # type: string
+    # choices: ['TPR/FPR', 'precision/recall']
+    answers['(c) Which metric?'] = 'TPR/FPR'
+
+    # type: explain_string
+    answers['(c) explain'] = "Precision/Recall cannot explain the discrepancy between TP and FP. The amount of FP increases along with the recall. Since TPR/FPR offers a statistic to highlight each classifier's random guessing component, it is more appropriate."
+    return answers
+
+
+#-----------------------------------------------------------
+def question7():
+    answers = {}
+
+    # type: string
+    # choices: ['C1', 'C2', 'None']
+    answers['(i) Best classifier?'] = 'C2'
+
+    # type: explain_string
+    answers['(i) Best classifier, explain'] = 'The precision of both classifiers is equal, however C2 has a greater recall/TPR and F-1 measure at the expense of a high FPR. This demonstrates that false positives are not harmful and that C2 is more adept at identifying positives. C1 would be preferable if there is a chance of false positives.'
+
+    # type: string
+    # choices: ['TPR-FPR', 'precision-recall-F1-Measure']
+    answers['(ii) appropriate metric pair'] = 'precision-recall-F1-Measure'
+
+    # type: explain_string
+    answers['(ii) appropriate metric pair, explain'] = 'A more thorough understanding of performance is provided by the Precision, Recall, and F-1 Measure, which compares the two classifiers proportionately.'
+
+    # type: string
+    # choices: ['C1', 'C2', 'C3']
+    answers['(iii) preferred classifier?'] = 'C3'
+
+    # type: explain_string
+    answers['(iii) best classifier, explain'] = 'In essence, C3 is an improved C1. It retains the low FPR of C1 but has higher precision (higher than C2 as well), F-1 measure, and recall. Even though these measurements are not as good as C2, I think the trade-off of having such a high FPR is worthwhile.' 
+    return answers
+
+
+#-----------------------------------------------------------
+def question8():
+    answers = {}
+
+    # type: eval_float
+    answers['(a) precision for C0'] = '(p * 100) / ((p * 100) + (p * 900))'
+
+    # type: eval_float
+    answers['(a) recall for C0'] = '(p * 100) / ((p * 100) + ((1-p) * 100))'
+
+    # type: eval_float
+    answers['(b) F-measure of C0'] = '2 * ((0.1 * p) / (0.1 + p))'
+
+    # type: string
+    # choices: ['yes', 'no', 'unknown']
+    answers['C1 better than random?'] = 'no'
+
+    # type: float
+    # What is the range of p for which C1 is better than random?  What is
+    # "?" in the expression "p > ?"
+
+    answers['p-range'] = 0.3
+    return answers
+
+
+#-----------------------------------------------------------
+def question9():
+    answers = {}
+
+    # type: dict[string,float]
+    # keys: ['recall', 'precision', 'F-measure', 'accuracy']
+    answers['(i) metrics'] = {
+        'recall': 0.533,
+        'precision': 0.615,
+        'F-measure': 0.571,
+        'accuracy': 0.88
+    }
+    # type: string
+    # choices: ['recall', 'precision', 'F-measure', 'accuracy']
+    answers['(i) best metric?'] = 'F-measure'
+
+    # type: string
+    # choices: ['recall', 'precision', 'F-measure', 'accuracy']
+    answers['(i) worst metric?'] = 'accuracy'
+
+    # type: explain_string
+    answers['(ii) Explain your choices of best and worst metrics'] = 'The F-measure is unquestionably the greatest metric of the three since it incorporates recall and precision into a single statistic. Because accuracy just takes into consideration the total number of mistakes rather than the type of mistake (i.e., does not account for the discrepancy between FP and FN in context), it can be severely skewed as a result of imbalanced classes.'
+    return answers
+
+
+#-----------------------------------------------------------
+def question10():
+    answers = {}
+
+    # type: string
+    # choices: ['T1', 'T2']
+    answers['(a) better test based on F-measure?'] = 'T1'
+
+    # type: string
+    # choices: ['T1', 'T2']
+    answers['(b) better test based on TPR/FPR?'] = 'T2'
+
+    # type: string
+    # choices: ['F1', 'TPR/FPR']
+    answers['(c) Which evaluation measure to use between the two tests?'] = 'TPR/FPR'
+    # type: explain_string
+    answers['(c) Which evaluation measure? Explain'] = 'It seems more serious than misdiagnosing someone, especially in light of the circumstances, to fail to diagnose someone with cancer. For this reason, the best metric to employ is TPR/FPR.'
+
+
+    # type: explain_string
+    answers['(d) Example scenario where you would reverse choise in (c)'] = 'When it comes to email spam detection, F-measure should be used instead of TPR/FPR since FP and FN are less serious and receiving a spam email does not necessarily mean someone will die.'
+    return answers
+#-----------------------------------------------------------
+if __name__ == '__main__':
+    answers_dict = {}
+    answers_dict['question1'] = question1()
+    answers_dict['question2'] = question2()
+    answers_dict['question3'] = question3()
+    answers_dict['question4'] = question4()
+    answers_dict['question5'] = question5()
+    answers_dict['question6'] = question6()
+    answers_dict['question7'] = question7()
+    answers_dict['question8'] = question8()
+    answers_dict['question9'] = question9()
+    answers_dict['question10'] = question10()
+
+    with open('answers.pkl', 'wb') as f:
+        pickle.dump(answers_dict, f)
